07807db7513adb33dd4605ae26c09f9250899c20
JE-DW <103127734+JE-DW@users.noreply.github.com>
2022-12-02 14:51:22 +0100

    4 files deleted

--- src/app/quoting.pipe.ts
+++
@@ -1,8 +0,0 @@
-import { Pipe, PipeTransform } from '@angular/core';
-
-@Pipe({ name: 'quoting'})
-export class QuotingPipe implements PipeTransform {
-  transform(value: string) {
-    return `"${value}"`;
-  }
-}
